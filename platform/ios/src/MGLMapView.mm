bd6fc9a0ef4ed0dca27ce86976913c9d8acaa838
Lloyd Sheng <i@lloydsheng.com>
2019-03-22 17:52:53 +0800

    Fix arrow direction issue

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -6281,8 +6281,19 @@ public:
 }
 
 - (void)updateAttributionAlertView {
-    if (self.attributionController && self.attributionController.presentingViewController) {
+    if (self.attributionController.presentingViewController) {
         self.attributionController.popoverPresentationController.sourceRect = self.attributionButton.frame;
+        switch (self.attributionButtonPosition) {
+            case MGLOrnamentPositionTopLeft:
+            case MGLOrnamentPositionTopRight:
+                [self.attributionController.popoverPresentationController setPermittedArrowDirections:UIMenuControllerArrowUp];
+                break;
+            case MGLOrnamentPositionBottomLeft:
+            case MGLOrnamentPositionBottomRight:
+                [self.attributionController.popoverPresentationController setPermittedArrowDirections:UIMenuControllerArrowDown];
+                break;
+        }
+        [self.attributionController.popoverPresentationController.containerView setNeedsLayout];
     }
 }
 
